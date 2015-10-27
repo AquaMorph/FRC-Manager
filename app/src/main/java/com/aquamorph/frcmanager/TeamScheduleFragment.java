4bbdc3b9fdf0dc1dde1ef93537788424aa09e052
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-10-27 19:37:18 -0400

    Added SwipeRefresh Updated Gradle version

--- app/src/main/java/com/aquamorph/frcmanager/TeamScheduleFragment.java
+++ app/src/main/java/com/aquamorph/frcmanager/TeamScheduleFragment.java
@@ -2,9 +2,11 @@ package com.aquamorph.frcmanager;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.Adapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +14,7 @@ import android.view.ViewGroup;
 public class TeamScheduleFragment extends Fragment {
 
 	private String TAG = "TeamScheduleFragment";
-//	private SwipeRefreshLayout mSwipeRefreshLayout;
+	private SwipeRefreshLayout mSwipeRefreshLayout;
 	private RecyclerView recyclerView;
 	private Adapter adapter;
 	public String[] data = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"};
@@ -26,8 +28,14 @@ public class TeamScheduleFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_team_schedule, container, false);
-//		mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
-//		mSwipeRefreshLayout.setOnRefreshListener(this);
+		mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
+		mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				mSwipeRefreshLayout.setRefreshing(false);
+			}
+		});
+
 
 		recyclerView = (RecyclerView) view.findViewById(R.id.rv);
 		adapter = new TeamScheduleAdapter(getContext(), data);
