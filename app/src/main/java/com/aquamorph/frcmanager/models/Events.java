e12a7d72a31cda72b989e92822416620f04a5af5
Christian Colglazier <aquamorphcomputing@gmail.com>
2016-01-19 11:09:33 -0500

    Added event model

---
+++ app/src/main/java/com/aquamorph/frcmanager/models/Events.java
@@ -0,0 +1,20 @@
+package com.aquamorph.frcmanager.models;
+
+public class Events {
+	public String key;
+	public String website;
+	public Boolean official;
+	public String end_date;
+	public String name;
+	public String short_name;
+	public String facebook_eid;
+	public String event_district_string;
+	public String venue_address;
+	public String event_district;
+	public String location;
+	public String event_code;
+	public String year;
+	public String event_type_string;
+	public String start_date;
+	public String event_type;
+}
