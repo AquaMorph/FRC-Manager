3203cccdb69d8c368434c6e92954ce617ff3e337
Christian Colglazier <aquamorphcomputing@gmail.com>
2016-03-11 11:04:37 -0500

    Added Event Team model

---
+++ app/src/main/java/com/aquamorph/frcmanager/models/EventTeam.java
@@ -0,0 +1,21 @@
+package com.aquamorph.frcmanager.models;
+
+/**
+ * Stores information about a team at an event.
+ *
+ * @author Christian Colglazier
+ * @version 3/11/2016
+ */
+public class EventTeam {
+	public String website;
+	public String name;
+	public String locality;
+	public String rookie_year;
+	public String region;
+	public String team_number;
+	public String location;
+	public String key;
+	public String country_name;
+	public String motto;
+	public String nickname;
+}
