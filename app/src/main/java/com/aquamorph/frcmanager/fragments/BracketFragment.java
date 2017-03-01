79233787cf75df5198f81761a2e77978e0b70cf5
Christian Colglazier <aquamorphcomputing@gmail.com>
2017-03-01 13:42:39 -0500

    Added loading quarterfinals from data

--- app/src/main/java/com/aquamorph/frcmanager/fragments/BracketFragment.java
+++ app/src/main/java/com/aquamorph/frcmanager/fragments/BracketFragment.java
@@ -42,6 +42,13 @@ public class BracketFragment extends Fragment implements
 	private String teamNumber = "", eventKey = "";
 	private Parser<ArrayList<Match>> parserMatch;
 	private Parser<Events> parserEvents;
+	private View qf18;
+	private View qf27;
+	private View qf36;
+	private View qf45;
+	private View sf1;
+	private View sf2;
+	private View f;
 
 	/**
 	 * newInstance creates and returns a new BracketFragment
@@ -76,6 +83,14 @@ public class BracketFragment extends Fragment implements
 			}
 		});
 
+		qf18 = view.findViewById(R.id.qf18);
+		qf27 = view.findViewById(R.id.qf27);
+		qf36 = view.findViewById(R.id.qf36);
+		qf45 = view.findViewById(R.id.qf45);
+		sf1 = view.findViewById(R.id.sf1);
+		sf2 = view.findViewById(R.id.sf2);
+		f = view.findViewById(R.id.f);
+
 		if (savedInstanceState == null) refresh();
 //		Constants.checkNoDataScreen(eventMatches, view, emptyView);
 		return view;
@@ -85,6 +100,37 @@ public class BracketFragment extends Fragment implements
 		if (!eventKey.equals("")) {
 			new BracketFragment.LoadBracket().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 		}
+	}
+
+	private void fillBracket(View view, Events.Alliances red, int rRank, Events.Alliances blue,
+	                         int bRank) {
+		TextView redRank = (TextView) view.findViewById(R.id.redRank);
+		TextView red1 = (TextView) view.findViewById(R.id.redTeam1);
+		TextView red2 = (TextView) view.findViewById(R.id.redTeam2);
+		TextView red3 = (TextView) view.findViewById(R.id.redTeam3);
+		TextView red4 = (TextView) view.findViewById(R.id.redTeam4);
+		TextView blueRank = (TextView) view.findViewById(R.id.blueRank);
+		TextView blue1 = (TextView) view.findViewById(R.id.blueTeam1);
+		TextView blue2 = (TextView) view.findViewById(R.id.blueTeam2);
+		TextView blue3 = (TextView) view.findViewById(R.id.blueTeam3);
+		TextView blue4 = (TextView) view.findViewById(R.id.blueTeam4);
+
+		redRank.setText(String.valueOf(rRank));
+		red1.setText(Constants.formatTeamNumber(red.picks[0]));
+		red2.setText(Constants.formatTeamNumber(red.picks[1]));
+		red3.setText(Constants.formatTeamNumber(red.picks[2]));
+		if (red.picks.length > 3)
+			red4.setText(Constants.formatTeamNumber(red.picks[3]));
+		else
+			red4.setVisibility(View.GONE);
+		blueRank.setText(String.valueOf(bRank));
+		blue1.setText(Constants.formatTeamNumber(blue.picks[0]));
+		blue2.setText(Constants.formatTeamNumber(blue.picks[1]));
+		blue3.setText(Constants.formatTeamNumber(blue.picks[2]));
+		if (red.picks.length > 3)
+			blue4.setText(Constants.formatTeamNumber(blue.picks[3]));
+		else
+			blue4.setVisibility(View.GONE);
 	}
 
 	@Override
@@ -145,6 +191,12 @@ public class BracketFragment extends Fragment implements
 //			Constants.checkNoDataScreen(eventMatches, recyclerView, emptyView);
 			filterMatches();
 			mSwipeRefreshLayout.setRefreshing(false);
+			if(alliances.size() >= 4) {
+				fillBracket(qf18, alliances.get(0), 1, alliances.get(7), 8);
+				fillBracket(qf27, alliances.get(1), 2, alliances.get(6), 7);
+				fillBracket(qf36, alliances.get(2), 3, alliances.get(5), 6);
+				fillBracket(qf45, alliances.get(3), 4, alliances.get(4), 5);
+			}
 		}
 	}
 }
