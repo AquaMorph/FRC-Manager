a56b935f7feb7ce13961c86a107672c71aca9751
Christian Colglazier <ccolglazier@lenovo.com>
2018-02-21 16:40:32 -0500

    Refactored code

--- app/src/main/java/com/aquamorph/frcmanager/network/DataLoader.java
+++ app/src/main/java/com/aquamorph/frcmanager/network/DataLoader.java
@@ -69,50 +69,5 @@ public class DataLoader {
 				"Alliance");
 	}
 
-	static class Load extends AsyncTask<Void, Void, Void> {
 
-		DataContainer dataContainer;
-		boolean isRank = false;
-		boolean isSortable = false;
-
-		Load(DataContainer dataContainer) {
-			this.dataContainer = dataContainer;
-		}
-
-		Load(DataContainer dataContainer, boolean isRank, boolean isSortable) {
-			this.dataContainer = dataContainer;
-			this.isRank = isRank;
-			this.isSortable = isSortable;
-		}
-
-		@Override
-		protected void onPreExecute() {
-			dataContainer.complete = false;
-		}
-
-		@Override
-		protected Void doInBackground(Void... params) {
-			dataContainer.parser.fetchJSON(true);
-			while (dataContainer.parser.parsingComplete) {
-				SystemClock.sleep(Constants.THREAD_WAIT_TIME);
-			}
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Void result) {
-			dataContainer.data.clear();
-			if (dataContainer.parser.getData() != null) {
-				if(isRank) {
-					dataContainer.data.add(dataContainer.parser.getData());
-				} else {
-					dataContainer.data.addAll((Collection) dataContainer.parser.getData());
-					if (isSortable) {
-						sort(dataContainer.data);
-					}
-				}
-			}
-			dataContainer.complete = true;
-		}
-	}
 }
